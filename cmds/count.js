3aad1712f4f80be03b7cf16955ed7a3f4d26da00
unknown <benjamin.caines243@gmail.com>
2019-01-20 09:45:15 +1100

    turd

--- cmds/count.js
+++ cmds/count.js
@@ -1,38 +1,33 @@
 const ytdl = require('ytdl-core');
- 
+
 module.exports.run = async (bot, message, args) => {
- 
+
     const streamOptions = {seek: 0, volume: 1};
-   
-    // put your voice channel id here
-    let voiceChannelID = "532144262557597696";
- 
-    console.log("Starting voice command");
- 
+    let voiceChannelID = "534890071606689792";
+    let id = 'https://youtu.be/y1eF7kX0oYc';
+    console.log("Starting Voice Command");
+    
     if (voiceChannelID != null) {
-        if (message.guild.channels.get(voiceChannelID)){
+        if(message.guild.channels.get(voiceChannelID)){
             let vc = message.guild.channels.get(voiceChannelID);
             console.log("Next stop, connection");
- 
+
             vc.join().then(connection => {
                 console.log("[VOICE CHANNEL] joined countdown channel.");
-                const stream = ytdl('https://www.youtube.com/watch?v=nyC0c6t7Vq0', {filter: 'audioonly'});
+                const stream = ytdl(id, {highWaterMark: 1024 * 1024 * 10 , quality: 'highestaudio'});
                 const dispatcher = connection.playStream(stream, streamOptions);
- 
+
                 dispatcher.on("end", end => {
                     console.log("[VOICE CHANNEL] left countdown channel.");
-                    vc.leave();
                 });
             }).catch(err => {
                 console.log(err);
             });
- 
+
         }
     }
 }
- 
- 
- 
+
 module.exports.help = {
     name: "count"
 }
